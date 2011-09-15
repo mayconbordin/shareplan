7b17e29ef3a2c6e1eb3a8fd91f982a1e54be2c67
Maycon Bordin <mayconbordin@gmail.com>
2011-09-15 12:10:35 -0300

    adicionada validações para items

--- app/models/income_statement_item.rb
+++ app/models/income_statement_item.rb
@@ -1,4 +1,9 @@
 class IncomeStatementItem < ActiveRecord::Base
+  validates_presence_of :income_statement, :item, :order
+  validates_length_of :function, :maximum => 255
+  validates_associated :income_statement, :item
+  validates_numericality_of :order, :value, :parent_id
+  
   belongs_to :income_statement
   belongs_to :item
   belongs_to :parent, :class_name => "IncomeStatementItem", :foreign_key => :parent_id
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,10 @@ class Item < ActiveRecord::Base
   TYPE_DEBT       = "debt"
   TYPE_CREDIT     = "credit"
   
+  validates_presence_of :name, :classification
+  validates_length_of :name, :maximum => 120
+  validates_length_of :classification, :item_type, :maximum => 30
+  validates_associated :user
+    
   belongs_to :user
 end
